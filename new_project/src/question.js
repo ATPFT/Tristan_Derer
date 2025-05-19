68719b62e13ed616445f3997020eb5a397cacce8
Tristan <tristan.derer@epita.fr>
2025-05-19 12:21:32 +0200

    ajout du quizz

---
+++ new_project/src/question.js
@@ -0,0 +1,32 @@
+const questionnaire = [
+  {
+    qlabel: "A quel point avez-vous aimée mon site ?",
+    qid: 1,
+    reponses: [
+      {rlabel: "Incroyable", rid: 1},
+      {rlabel: "Plutôt bien", rid: 2},
+      {rlabel: "Pas fou", rid: 3},
+      {rlabel: "Horrible", rid: 4}
+    ]
+  },
+  {
+    qlabel: "Quelle note allez-vous me mettre ?",
+    qid: 2,
+    reponses: [
+      {rlabel: "5", rid: 1},
+      {rlabel: "10", rid: 2},
+      {rlabel: "15", rid: 3},
+      {rlabel: "20", rid: 4}
+    ]
+  },
+  {
+    qlabel: "Si vous etiez recruteur m'auriez-vous pris ?",
+    qid: 3,
+    reponses: [
+      {rlabel: "Evidemment", rid: 1},
+      {rlabel: "Pourquoi pas", rid: 2},
+      {rlabel: "En dernier recours", rid: 3},
+      {rlabel: "Absolument pas", rid: 4}
+    ]
+  }
+];
